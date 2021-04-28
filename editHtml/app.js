68c5f920090f3943b75b3094efcb090ccf7bc49e
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-04-28 10:52:35 +0300

    more

--- editHtml/app.js
+++ editHtml/app.js
@@ -10,6 +10,18 @@
 
 // 5. pakeisti article viduje antrastes texta
 
-// 6. pakeisti article paragrafo teksto dydi i 20px
+// 6. pakeisti article paragrafu teksto dydi i 20px
 
 // 7 prie esamos Pagr antrastes prideti teksta '(pakeista)'
+
+// 8 pakeisti paskutinio article paragrafo texto srifta i Verdana
+
+// 9. padaryti kad tik tiesioginiai article paragrafai butu 20px dydzio
+
+// 10. prideti prie aside article vidue clase "inner-aside"
+
+// 11. prideti prie aside nesancios article viduej klase 'main-aside'
+
+// 12. padaryti kad visi main-aside el butu 18to dydzio ir melynos spalvos
+
+//
