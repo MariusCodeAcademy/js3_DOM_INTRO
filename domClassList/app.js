a90629a58dbc74d9bc7c285c0d29217e20b3c3e3
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-04-29 09:07:09 +0300

    tr

--- domClassList/app.js
+++ domClassList/app.js
@@ -18,5 +18,10 @@ function toggleUlVisibility() {
   //   ul.classList.toggle("hide");
   ul.classList.add("testing");
   //   ul.classList.remove("test2");
-  ul.classList.replace("test2", "test17");
+  //   ul.classList.replace("test2", "test17");
+  if (ul.classList.contains("test2")) {
+    console.log("Yest there is test2 class");
+  } else {
+    console.log("test2 class not found");
+  }
 }
