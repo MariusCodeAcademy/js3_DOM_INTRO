b02c2343292992d2c2c52ca7a1176175a83c6591
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-04-29 13:35:58 +0300

    practiceTasks

--- createHtmlTasks/app.js
+++ createHtmlTasks/app.js
@@ -26,3 +26,22 @@ let colors = ["blue", "green", "orange", "tomato", "red"];
 
 // 5.1 prideti 2 paragrafus su lorem tekstais kurie turetu klases
 // '.main-article__text'
+
+// 6. po main-article sukurti aside elmenta su klase 'info'
+// jame sukurti nav elementa. nav elemente sugeneruoti nuorodu sarasa
+// su nuorodom vedandciom i about.html, contact.html, home.html
+
+// 7. po aside.info el suskurti div su klase modal
+// modal viduje sukurti h2 antraste su tekstu 0
+// ir 2 mygtukus
+// vienas mygtukas butu add
+// kitas subtract
+// jie turetu padidinti ir pamazinti h2 antrastes skaitliuka
+// aprasyti .modal klase stiliaus faile taip kad jis butu nematomas
+// ekrano centre 20 x 20 proc ekrano dydzio
+// turetu juoda apvada ir butu centruotas ekravo viduryje
+
+// 8.sukurti mygtuka headeryje su tekstu 'modal', ji paspaudus turetu pasirodyti
+// arba pasislepti modal. (prideti nuimti klase? )
+
+//
